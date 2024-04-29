dfb8a1fdaaa52313c737c592a1a3d53ce3b5d8c9
adamcanray <adamcool180@gmail.com>
2024-04-30 01:48:07 +0700

    mongo util

--- utils/mongodb.py
+++ utils/mongodb.py
@@ -3,9 +3,12 @@ from utils import config
 
 config.init()
 
-def connect_mongodb():
-  client = MongoClient(config.mongodb_url)
-
-  db = client['pacs']
-  collection = db['dicom_metadata']
+def connect_mongodb(coll, client=None):
+  if not client:
+      client = MongoClient(config.mongodb_url)
+  db = client['pacs_live']
+  collection = db[coll if coll else "dicom_metadata"]
   return collection
+
+def client_mongodb():
+  return MongoClient(config.mongodb_url)
