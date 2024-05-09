ef9ec9c96759e3c86d62fda0db6603b08a98fd72
adamcanray <adamcool180@gmail.com>
2024-05-09 22:14:13 +0700

    enable oauth2 satusehat code

--- utils/oauth2.py
+++ utils/oauth2.py
@@ -12,21 +12,18 @@ secret_key = config.get('satusehat', 'secret_key')
 
 def get_token():
 
-  # global token
-  # payload = 'client_id='+client_key+'&client_secret='+secret_key
-  # headers = {
-  #   'Content-Type': 'application/x-www-form-urlencoded'
-  # }
-  # try:
-  #   res = requests.post(url+"/oauth2/v1/accesstoken?grant_type=client_credentials", data=payload, headers=headers)
-  #   # data = res.json()
-  #   data = res if res.status_code != 200 else res.json()
-  # except Exception as e: # work on python 3.x
-  #   LOGGER.exception(e)
-  #   LOGGER.error("Authentication failed")
-  #   return ""
-  # token = data["access_token"]
-  # return data["access_token"]
-
-  return "fake_token"
-
+  global token
+  payload = 'client_id='+client_key+'&client_secret='+secret_key
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  try:
+    res = requests.post(url+"/oauth2/v1/accesstoken?grant_type=client_credentials", data=payload, headers=headers)
+    # data = res.json()
+    data = res if res.status_code != 200 else res.json()
+  except Exception as e: # work on python 3.x
+    LOGGER.exception(e)
+    LOGGER.error("Authentication failed")
+    return ""
+  token = data["access_token"]
+  return data["access_token"]
