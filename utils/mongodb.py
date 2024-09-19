6c50c3f79e87cdd9eff75ec65283c5d665e2798c
heryanto87 <heryantoliao8@gmail.com>
2024-09-20 03:24:20 +0700

    add certificate

--- utils/mongodb.py
+++ utils/mongodb.py
@@ -1,32 +1,37 @@
 from pymongo import MongoClient
 from utils import config
+import certifi
+import os
 
 # Initialize configuration
 config.init()
 
 def connect_mongodb(coll=None, client=None):
-    """
-    Connect to a MongoDB collection.
+		"""
+		Connect to a MongoDB collection.
 
-    Args:
-        coll (str, optional): The name of the collection to connect to. Defaults to "dicom_metadata".
-        client (MongoClient, optional): An existing MongoClient instance. If not provided, a new instance is created.
+		Args:
+				coll (str, optional): The name of the collection to connect to. Defaults to "dicom_metadata".
+				client (MongoClient, optional): An existing MongoClient instance. If not provided, a new instance is created.
 
-    Returns:
-        Collection: A MongoDB collection object.
-    """
-    if client is None:
-        client = MongoClient(config.mongodb_url)
+		Returns:
+				Collection: A MongoDB collection object.
+		"""
+		mongodb_url = os.getenv('MONGODB_URL', config.mongodb_url)
+		db_name = os.getenv('PACS_DB_NAME', config.pacs_db_name)
+		if client is None:
+				client = MongoClient(mongodb_url,  tlsCAFile=certifi.where())
 
-    db = client[config.pacs_db_name]
-    collection_name = coll or "dicom_metadata"  # Use default collection if coll is None
-    return db[collection_name]
+		db = client[db_name]
+		collection_name = coll or "dicom_metadata"  # Use default collection if coll is None
+		return db[collection_name]
 
 def client_mongodb():
-    """
-    Create and return a new MongoClient instance.
+		"""
+		Create and return a new MongoClient instance.
 
-    Returns:
-        MongoClient: A new MongoClient connected to the MongoDB server.
-    """
-    return MongoClient(config.mongodb_url)
+		Returns:
+				MongoClient: A new MongoClient connected to the MongoDB server.
+		"""
+		mongodb_url = os.getenv('MONGODB_URL', config.mongodb_url)
+		return MongoClient(mongodb_url, tlsCAFile=certifi.where())
